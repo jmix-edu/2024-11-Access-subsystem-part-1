497d635c74f87d0a4893e13cee3452a85a1937b3
vashenkoda <d.vaschenko@haulmont.net>
2024-11-25 19:47:43 +0300

    LDAP with JPA User sync

---
+++ src/main/java/com/company/jmixpm/ldap/PmUserSyncStrategy.java
@@ -0,0 +1,21 @@
+package com.company.jmixpm.ldap;
+
+import com.company.jmixpm.entity.User;
+import io.jmix.ldap.userdetails.AbstractLdapUserDetailsSynchronizationStrategy;
+import org.springframework.ldap.core.DirContextOperations;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PmUserSyncStrategy extends AbstractLdapUserDetailsSynchronizationStrategy<User> {
+    @Override
+    protected Class<User> getUserClass() {
+        return User.class;
+    }
+
+    @Override
+    protected void mapUserDetailsAttributes(User userDetails, DirContextOperations ctx) {
+        userDetails.setFirstName(ctx.getStringAttribute("givenName"));
+        userDetails.setLastName(ctx.getStringAttribute("sn"));
+        userDetails.setEmail(ctx.getStringAttribute("mail"));
+    }
+}
